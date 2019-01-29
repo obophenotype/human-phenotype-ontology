a46a6e73ed08afc36be9a466ff1ea366e1f4c523
matentzn <nicolas.matentzoglu@gmail.com>
2019-01-29 12:55:55 +0000

    add pr back in

--- src/ontology/run.sh
+++ src/ontology/run.sh
@@ -9,4 +9,4 @@
 # we therefore map the whole repo (../..) to a docker volume.
 #
 # See README-editors.md for more details.
-docker run -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/odkfull "$@"
+docker run -v $PWD/../../:/work -w /work/src/ontology -e ROBOT_JAVA_ARGS='-Xmx8G' --rm -ti obolibrary/odkfull "$@"
