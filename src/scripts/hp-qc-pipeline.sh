ead478d94afde35ced9a89d0cf98dee06313e88d
drseb <dr.sebastian.koehler@gmail.com>
2021-09-22 20:16:31 +0200

    fix the right script hopefully

--- src/scripts/hp-qc-pipeline.sh
+++ src/scripts/hp-qc-pipeline.sh
@@ -6,7 +6,7 @@ ONTDIR=$1
 TMP_DIR=$ONTDIR/tmp
 HP_EDIT=$ONTDIR/hp-edit.owl
 HP_OBO=$ONTDIR/hp.obo
-OBO_CHECK_JAR=$TMP_DIR/hpo-obo-qc/target/hpo-obo-qc-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+OBO_CHECK_JAR=$TMP_DIR/performHpoOboQc.jar
 OWL_CHECK_JAR=$TMP_DIR/hpo-owl-qc/target/hpo-owl-qc-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 JAVA_OPTS=-Xmx4g
 
@@ -22,20 +22,18 @@ mkdir -p $TMP_DIR
 rm -rf $TMP_DIR/hpo-owl-qc
 
 cd $TMP_DIR
-echo "Downloading dependencies.."
+echo "Downloading dependencies owl-qc..."
 git clone https://github.com/Phenomics/hpo-owl-qc.git --quiet
 cd hpo-owl-qc
 mvn clean install --quiet
+echo "Downloading dependencies obo-qc..."
 cd $HOME_DIR
-rm -rf $TMP_DIR/hpo-obo-qc
 cd $TMP_DIR
-git clone https://github.com/Phenomics/hpo-obo-qc.git --quiet
-cd hpo-obo-qc
-mvn clean install --quiet
+wget https://github.com/Phenomics/hpo-build-jars/raw/master/performHpoOboQc.jar
 
 cd $HOME_DIR
 
-echo "Running tests.."
+echo "Running tests..."
 # encoding test on hp-edit
 iconv -f UTF-8 -t ISO-8859-15 $HP_EDIT > $TMP_DIR/converted.txt || (echo "found special characters in ontology. remove those!"; exit 1)
 # java-based owl checks on hp-edit
