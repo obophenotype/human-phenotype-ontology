ebdc8022b3d330e4cd5b5563c0c6616162c4b932
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2022-12-15 19:44:24 +0200

    Update build-without-imports.sh

--- src/ontology/build-without-imports.sh
+++ src/ontology/build-without-imports.sh
@@ -1,4 +1,8 @@
 # This script builds the HPO completely without imports
+
+set -e
+
 sh run.sh make hpoa_clean -B
+test tmp/hpoa/hpo-annotation-data/README.md
 sh run.sh make IMP=false prepare_release -B
 sh run.sh make hpoa -B
