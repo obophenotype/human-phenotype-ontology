46ef6fc041b556f4cf1210e870d20772d9385868
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2023-06-07 18:10:18 +0300

    Update build-without-imports.sh

--- src/ontology/build-without-imports.sh
+++ src/ontology/build-without-imports.sh
@@ -3,7 +3,7 @@
 set -e
 
 sh run.sh make hpoa_clean -B
-test tmp/hpoa/hpo-annotation-data/README.md
+test -f tmp/hpo-annotation-data/README.md
 sh run.sh make IMP=false prepare_release -B
 sh run.sh make translations/hp-all.babelon.tsv -B
 sh run.sh make hpoa -B
